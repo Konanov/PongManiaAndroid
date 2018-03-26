1f028dc78e3116967894a612c3b445f6e2b7561e
konanovmaxim <konanovmaxim@gmail.com>
2018-03-27 00:28:43 +0300

    - picasso test

--- app/src/main/java/com/pongmania/konanov/adapter/PlayerMainAdapter.kt
+++ app/src/main/java/com/pongmania/konanov/adapter/PlayerMainAdapter.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageSwitcher
+import android.widget.ImageView
 import android.widget.TextView
 import com.pongmania.konanov.R
 import com.pongmania.konanov.model.Player
+import com.squareup.picasso.Picasso
+import java.io.File
 
 class PlayerMainAdapter(private val context: Context,
                         private val dataSource: ArrayList<Player>) : BaseAdapter() {
@@ -17,6 +21,11 @@ class PlayerMainAdapter(private val context: Context,
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val rowView = inflater.inflate(R.layout.player_row, parent, false)
         // Get title element
+        val avatar = rowView.findViewById(R.id.recipe_list_thumbnail) as ImageView
+        var picasso = Picasso.get()
+        picasso.isLoggingEnabled = true
+        picasso.load("http://leo-realty.com.ua/assets/missing_agent-fa85f1d8af2ca785aaaceae27c7670a7ad93bbcee0a3e20f33d37090842c9c06.png").into(avatar)
+
         val userNameTitle = rowView.findViewById<View>(R.id.userName_title) as TextView
         val userName = rowView.findViewById<View>(R.id.userName_subtitle) as TextView
         // Get subtitle element
