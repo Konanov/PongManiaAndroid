3a10ee7c62a6ca0c7169d9aa55e6ff2b81a65c13
konanovmaxim <konanovmaxim@gmail.com>
2018-04-09 23:30:24 +0300

    - minor changes

--- app/src/main/java/com/pongmania/konanov/activity/PlayerProfileActivity.kt
+++ app/src/main/java/com/pongmania/konanov/activity/PlayerProfileActivity.kt
@@ -2,8 +2,11 @@ package com.pongmania.konanov.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.TextView
 import com.pongmania.konanov.R
 import com.pongmania.konanov.model.Player
+import com.squareup.picasso.Picasso
 
 class PlayerProfileActivity : AppCompatActivity() {
 
@@ -14,5 +17,13 @@ class PlayerProfileActivity : AppCompatActivity() {
         val player = intent.getSerializableExtra("currentPlayer") as Player
         val name = "${player.credentials.firstName} ${player.credentials.lastName}"
         title = name
+
+        val ratingValue = findViewById<TextView>(R.id.playerRatingValue)
+        ratingValue.text = player.latestRating.rating
+
+        val avatar = findViewById<ImageView>(R.id.profile_image_main)
+        val picasso = Picasso.get()
+        picasso.isLoggingEnabled = true
+        picasso.load(R.drawable.placeholder).into(avatar)
     }
 }
