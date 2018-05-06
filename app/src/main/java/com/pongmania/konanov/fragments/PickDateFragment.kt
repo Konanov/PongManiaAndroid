d74f820a6721b3932bb1c871675dbaa90c829eab
Konanov Maxim <konanovmaxim@gmail.com>
2018-05-07 02:05:51 +0300

    - matchWinRation to be applied

--- app/src/main/java/com/pongmania/konanov/fragments/PickDateFragment.kt
+++ app/src/main/java/com/pongmania/konanov/fragments/PickDateFragment.kt
@@ -36,7 +36,6 @@ class PickDateFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-
         val view = inflater!!.inflate(R.layout.fragment_plan_date, container, false)
         ButterKnife.bind(this, view)
 
